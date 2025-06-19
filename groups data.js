b408ebf9d7bca79b28d60c7cc1b37eb8b6a7a9de
NadinZeitoune <42942222+NadinZeitoune@users.noreply.github.com>
2025-06-19 17:18:16 +0300

    Fixes & features

---
+++ groups data.js
@@ -0,0 +1,19 @@
+const groupsNames = [
+"Base Game",
+"Seasons",
+"City Living",
+"Cats & Dogs",
+];
+
+const groups = [
+	['BG1', 'BG2', 'BG3', 'BG4', 'BG5'],
+	['Seasons1', 'Seasons2', 'Seasons3', 'Seasons4'],
+	['City Living1', 'City Living2'],
+	['Cats & Dogs1', 'Cats & Dogs2', 'Cats & Dogs3', 'Cats & Dogs4'],
+];
+
+const secretMessages = {
+ 'City Living1': { message: 'Keep up the great work, Alice!', url: 'https://google.com' },
+};
+
+const numOfGroups = groupsNames.length;
